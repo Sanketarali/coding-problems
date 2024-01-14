9fe60f20089583f317d6dfca3ebd1758b4e497f6
Sanket Arali <110754364+Sanketarali@users.noreply.github.com>
2024-01-14 17:58:40 +0530

    Add files via upload

---
+++ changecase.py
@@ -0,0 +1,8 @@
+def change_case(string):
+    changed_string = string.swapcase()
+    return changed_string
+
+
+my_string = "Hello, World! Sanket"
+result = change_case(my_string)
+print("Changed case:", result)
