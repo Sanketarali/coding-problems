f3d9a8e0857ca45c3508b6d9a15c9f1034f18b64
Sanket Arali <110754364+Sanketarali@users.noreply.github.com>
2024-01-24 17:56:47 +0530

    Add files via upload

---
+++ lowercase_to_uppercase.py
@@ -0,0 +1,7 @@
+def convert_lowercase_to_uppercase(input_string):
+    return input_string.upper()
+
+# Example usage:
+input_string = "Hello, World!"
+result = convert_lowercase_to_uppercase(input_string)
+print(result)
