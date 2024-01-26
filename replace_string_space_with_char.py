92938e1f0e0108e6b21308ee43c99bb33f15b7ff
Sanket Arali <110754364+Sanketarali@users.noreply.github.com>
2024-01-26 18:38:13 +0530

    Add files via upload

---
+++ replace_string_space_with_char.py
@@ -0,0 +1,8 @@
+def replace_spaces(input_string, replacement_char):
+    return input_string.replace(' ', replacement_char)
+
+# Example usage:
+input_string = "Hello World"
+replacement_char = '_'
+result = replace_spaces(input_string, replacement_char)
+print(result)
